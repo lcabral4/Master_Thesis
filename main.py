aeb44602eaabb34f4fbcf91ce3c5550eaf75b879
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2021-11-29 21:30:33 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -101,16 +101,46 @@ for i in range(N):
 #    print(mag)
 #    [S11, S21, S12,S22]
 #    y = np.append(mag)
-    plt.xlabel('Frequancy (Hz')
-    plt.ylabel('Log Magnitude (dB')
-    plt.plot(x, mag)
+    plt.xlabel('Frequancy (Hz)')
+    plt.ylabel('Log Magnitude (dB)')
+#    plt.plot(x, mag)
+    if i == 0:
+        plt.plot(x, mag, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 4:
+        plt.plot(x, mag, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 8:
+        plt.plot(x, mag, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 12:
+        plt.plot(x, mag, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 16:
+        plt.plot(x, mag, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.title(name_dict[i])
     fig = plt.figure(11)
     SER = -10* log10(1 - np.abs(S11)**2)
-    plt.plot(x, SER)
+#    plt.plot(x, SER)
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('SER (dB)')
     plt.title('Shielding Due To Reflection')
+    if i == 0:
+        plt.plot(x, SER, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 4:
+        plt.plot(x, SER, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 8:
+        plt.plot(x, SER, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 12:
+        plt.plot(x, SER, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 16:
+        plt.plot(x, SER, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 #print(f)
 for i in range(N):
     if i == 0:
@@ -157,8 +187,23 @@ for i in range(N):
 #    y = np.append(mag)
     plt.xlabel('Frequancy (Hz')
     plt.ylabel('Log Magnitude (dB')
-    plt.plot(x, mag)
+#    plt.plot(x, mag)
     plt.title(name_dict[i])
+    if i == 1:
+        plt.plot(x, mag, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 5:
+        plt.plot(x, mag, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 9:
+        plt.plot(x, mag, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 13:
+        plt.plot(x, mag, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 17:
+        plt.plot(x, mag, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     
 for i in range(N):
     if i == 0:
@@ -207,13 +252,44 @@ for i in range(N):
 #    y = np.append(mag)
     plt.xlabel('Frequancy (Hz)')
     plt.ylabel('Log Magnitude (dB)')
-    plt.plot(x, mag)
+#    plt.plot(x, mag)
+    if i == 2:
+        plt.plot(x, mag, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 6:
+        plt.plot(x, mag, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 10:
+        plt.plot(x, mag, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 14:
+        plt.plot(x, mag, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 18:
+        plt.plot(x, mag, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.title(name_dict[i])
     fig = plt.figure(9)
-    plt.plot(x, Total_SE)
     plt.title('Total SE')
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Total SE (dB)')
+#    plt.legend((Total_SE[i]),loc='center left')
+        
+    if i == 2:
+        plt.plot(x, Total_SE, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 6:
+        plt.plot(x, Total_SE, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 10:
+        plt.plot(x, Total_SE, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 14:
+        plt.plot(x, Total_SE, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 18:
+        plt.plot(x, Total_SE, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 for i in range(N):
     if i == 0:
         continue
@@ -260,8 +336,23 @@ for i in range(N):
 #    y = np.append(mag)
     plt.xlabel('Frequancy (Hz')
     plt.ylabel('Log Magnitude (dB')
-    plt.plot(x, mag)
+#    plt.plot(x, mag)
     plt.title(name_dict[i])
+    if i == 3:
+        plt.plot(x, mag, label = 'Sample1')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 7:
+        plt.plot(x, mag, label = 'Sample2')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 11:
+        plt.plot(x, mag, label = 'Sample3')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 15:
+        plt.plot(x, mag, label = 'Sample4')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if i == 19:
+        plt.plot(x, mag, label = 'Sample5')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 #plt.show()
 
 
@@ -361,19 +452,26 @@ for i in range(N):
         plt.title('SE Due To Absorption')
         plt.xlabel('Frequancy (Hz)')
         plt.ylabel('SEA (dB)')
-        plt.plot(x, SEA)
+#        plt.plot(x, SEA)
+        if j == 2:
+            plt.plot(x, mag, label = 'Sample1')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if j == 6:
+            plt.plot(x, mag, label = 'Sample2')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if j == 10:
+            plt.plot(x, mag, label = 'Sample3')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if j == 14:
+            plt.plot(x, mag, label = 'Sample4')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if j == 18:
+            plt.plot(x, mag, label = 'Sample5')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
 #        plt.title(name_dict[i])
         
 
 plt.show()
 
 
-
-
-
-
-        
-        
-    
-
-        
